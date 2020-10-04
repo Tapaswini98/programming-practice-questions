5062382c668284754d46acb166624719aeceaf94
abhijeetmurmu1997 <abhijeetmurmu1997@gmail.com>
2020-10-04 14:48:39 +0530

    adding c code in java programming section

---
+++ DynamicProgramming/c coding/Untitledj11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int i,j;
+int main()
+{
+	for(i=5;i>=1;i--)
+	{
+		for(j=5;j>=1;j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ DynamicProgramming/c coding/Untitledj13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int i,j;
+int main()
+{
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=5;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
